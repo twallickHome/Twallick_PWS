93001c27e62f6797b07c440dfd44d447f7704316
twallickHome <twallick.home@gmail.com>
2019-03-09 13:11:41 -0500

    Revert "Compleated home page"
    This reverts commit c6e4ea5eb9877ed3e23e421fa9fa3ebcb26c974f.

---
+++ Ted Wallick V2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ted_Wallick_V2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
