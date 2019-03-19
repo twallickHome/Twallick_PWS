a44be393dabd7e5dc448aef415ef9fde06894c0b
twallickHome <twallick.home@gmail.com>
2019-03-19 14:02:48 -0400

    Work on Contact page

--- Ted Wallick/Pages/Contact.cshtml.cs
+++ Ted Wallick/Pages/Contact.cshtml.cs
@@ -1,18 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Ted_Wallick.Pages
 {
     public class ContactModel : PageModel
     {
+        [BindProperty]
+        public ContactFormModel Contact { get; set; }
+
         public string Message { get; set; }
 
         public void OnGet()
         {
-            Message = "Your contact page.";
+            Message = "Please send a message to me";
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var mailbody = $@"Hallo website owner, This is a new contact request from your website: Name: {Contact.Name} LastName: {Contact.LastName} Email: {Contact.Email} Message: ""{Contact.Message}"" Cheers, The websites contact form";
+            
+
+            if (!ModelState.IsValid)
+            {
+                
+
+                return Page();
+            }
+
+            //Send the message
+            SendMail(mailbody);
+
+            return RedirectToPage("Index");
+        }
+
+        private void SendMail(string mailbody)
+        {
+            using (var message = new MailMessage(Contact.Email, "twallick.home@gmail.com"))
+            {
+                message.To.Add(new MailAddress("twallick.home@gmail.com"));
+                message.From = new MailAddress(Contact.Email);
+                message.Subject = "New E-Mail from my website";
+                message.Body = mailbody;
+                using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtpClient.EnableSsl = true;
+                    smtpClient.Credentials = new System.Net.NetworkCredential("twallick.home@gmail.com", "mubrhtzbkqvpaneo");
+                    smtpClient.Send(message);
+                }
+            }
+        }
+
+       
     }
+
+    public class ContactFormModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+
 }
